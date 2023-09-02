7ba160fd3f5f8ca928fc67e2b4b7e3193f159cf9
Daniil <daniilkukotsky@gmail.com>
2023-09-02 16:54:59 +0400

    a method for creating an array has been created (+common output sections)

---
+++ FinalBlock1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+/*
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+
+
+string [] Create_UserArray (int count_el)
+{
+    string [] array = new string [count_el];
+    for (int i = 0; i <= count_el - 1; i++)
+    {
+        Console.WriteLine("Enter the element: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+
+
+
+Console.WriteLine("Enter the length of your array: ");
+
+int usercount = Convert.ToInt32(Console.ReadLine());
+string [] array = Create_UserArray(usercount);
+
+Console.WriteLine($"You array:" + "[" + string.Join(",", array) + "]");
+
